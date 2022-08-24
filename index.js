891d728ee268638a73dae3c4fb224e01556e8f73
MenaMema <amenabarrera@gmail.com>
2022-08-24 15:52:59 +0200

    finished

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 class Employee {
-	constructor (firstname, lastname) {
-		this.firstname = firstname
-		this.lastname = lastname
-		// Complete the code!
-	}
+  constructor(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.email = `${this.firstname.toLowerCase()}.${this.lastname.toLowerCase()}@company.com`;
+    // Complete the code!
+    this.fullname = ` ${this.firstname} ${this.lastname};`;
+  }
 }
+
+const newName = new Employee("Antonio", "Mena");
+console.log(newName);
+
+console.log("----------------------");
+
+console.log(newName.email);
+
+console.log("----------------------");
